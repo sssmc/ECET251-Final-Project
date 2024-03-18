50b305ee2a1569c3d5261a8e04b6b72bdae4bd5b
sebastien <sebastien@robitaille.info>
2024-03-18 09:25:48 -0700

    Comments and cleanup

--- ECET251-Final-Project-RX/src/main.cpp
+++ ECET251-Final-Project-RX/src/main.cpp
@@ -1,29 +1,13 @@
 #include <Arduino.h>
 
-#include <Manchester.h>
 
-/*
-
-  Manchester Receiver example
-  
-  In this example receiver will receive array of 10 bytes per transmittion
-
-  try different speeds using this constants, your maximum possible speed will 
-  depend on various factors like transmitter type, distance, microcontroller speed, ...
-
-  MAN_300 0
-  MAN_600 1
-  MAN_1200 2
-  MAN_2400 3
-  MAN_4800 4
-  MAN_9600 5
-  MAN_19200 6
-  MAN_38400 7
-
-*/
-
+//Address to receive data on
 #define ADDRESS 0xA4
 
+//Baudrate for serial connection
+#define BAUDRATE 115200
+
+//Pin definitions
 #define RX_PIN 3
 
 #define RED_LED 12
@@ -33,20 +17,25 @@
 #define RGB_LED_GREEN 10
 #define RGB_LED_BLUE 9
 
-#define BAUDRATE 115200
-
-
+//Size of the buffer for the received data
 #define BUFFER_SIZE 6
 
+//Create receive buffer
 uint8_t RXBuffer[BUFFER_SIZE];
 
-uint8_t Rx_num=0;
+//Recevied packet count
+uint8_t receivedPacketCount = 0;
 
-uint8_t LEDColors[3] = {0,0,0};
+//Function Prototypes
 
 uint8_t CalculateChecksum(uint8_t data[5]);
 
 void setup() {
+
+  //Start the serial connection
+  Serial.begin(BAUDRATE);
+
+  //Pin Modes
   pinMode(RED_LED, OUTPUT); 
   pinMode(GREEN_LED, OUTPUT);
   pinMode(RX_PIN, INPUT);
@@ -55,14 +44,14 @@ void setup() {
   pinMode(RGB_LED_GREEN, OUTPUT);
   pinMode(RGB_LED_BLUE, OUTPUT);
 
-  Serial.begin(BAUDRATE);
-
+  //Turn off all LEDs to start
   digitalWrite(RED_LED, 0);
   digitalWrite(GREEN_LED, 0);
   digitalWrite(RGB_LED_RED, 0);
   digitalWrite(RGB_LED_GREEN, 0);
   digitalWrite(RGB_LED_BLUE, 0);
 
+  //Setup the Manchester library to receive data
   man.setupReceive(RX_PIN, MAN_300 );
   man.beginReceiveArray(BUFFER_SIZE, RXBuffer);
 
@@ -71,14 +60,14 @@ void setup() {
 
 void loop() {
 	
-	
+	//If we have received a packet
   if (man.receiveComplete()) {
-	  
-	  Rx_num++;
+	  //Increment the received packet count
+	  receivedPacketCount++;
+    //Print the received packet count
 	  Serial.print("Received... message # ");
-	  Serial.println(Rx_num);
+	  Serial.println(receivedPacketCount);
 	  digitalWrite(RED_LED, 1);
-	  
     
     man.beginReceiveArray(BUFFER_SIZE, RXBuffer);
     for(int i=0; i<BUFFER_SIZE; i++){
